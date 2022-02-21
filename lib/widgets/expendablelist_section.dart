989942b57d65952b18abe8e994200a6bcba3cd4e
Yoon Joo <yoonjoo3615@gmail.com>
2022-02-21 13:30:38 +0100

    ExpansionPanel and ExpansionPanelList

--- lib/widgets/expendablelist_section.dart
+++ lib/widgets/expendablelist_section.dart
@@ -6,21 +6,32 @@ class ExpandableListSection implements ListItem {
   final String title;
 
   ExpandableListSection(this.title);
+
   //Expandable List starts.
 
   @override
   Widget buildBody(BuildContext context) {
-    return ListTile(
-      leading: Icon(Icons.keyboard_arrow_right),
-      title: Text(
-        title.toUpperCase(),
-        style: TextStyle(
-          fontSize: 15,
-          fontWeight: FontWeight.bold,
-          color: Colors.black45,
+    //bool isOpen;
+    return ExpansionPanelList(
+      animationDuration: Duration(seconds: 1),
+      dividerColor: Colors.grey,
+      expandedHeaderPadding: EdgeInsets.all(5),
+      children: [
+        ExpansionPanel(
+          canTapOnHeader: true,
+          headerBuilder: (context, isOpen) {
+            return Text(
+              title.toUpperCase(),
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+                color: Colors.black45,
+              ),
+            );
+          },
+          body: Text( 'Item of Expandable List'),
         ),
-      ),
+      ],
     );
   }
-
 }
